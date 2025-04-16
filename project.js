f6638d25c40a92dc19cdcf6c33f388503e9ae09d
Thomas Richard <Th.RICHARD2@outlook.com>
2025-04-16 11:31:09 +0200

    Refactor code structure for improved readability and maintainability

--- project.js
+++ project.js
@@ -18,12 +18,14 @@ const getProjects = (id) => {
     const titleContainer = document.querySelector('.row-2.col-2 .grid-1');
     const imageContainer = document.querySelector('.image-container');
     const technoContainer = document.querySelector('.row-2.col-1 .grid-3');
+    const linkContainer = document.querySelector('.row-2.col-1 .grid-4');
 
     titleContainer.innerHTML = `<h2 class="project-title"><span class="blinking">_</span>${projectStorage[id].name}</h2>
     <p class="descr">${projectStorage[id].desc}</p>
     `
     imageContainer.innerHTML = `<img class="project-image-alt" src="${projectStorage[id].image_alt}" alt="${projectStorage[id].name}"/>`;
     technoContainer.innerHTML = `<p>Technologies utilisées :</p><p class="techologies">${projectStorage[id].techno}</p>`;
+    linkContainer.innerHTML = `<p>Liens :</p><a class="link" href="${projectStorage[id].github}" target="_blank">Github</a>`;
 };
 
 
